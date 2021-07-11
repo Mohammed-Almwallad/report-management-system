663ab744a4ec5fcc829eda638deb430f18c58271
Mohammed-Almwallad <almwallad734@hotmail.com>
2021-07-11 21:49:44 +0300

    added groups table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,10 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class);
     }
 
+    public function groups(){
+        return $this->belongsToMany(Group::class);
+    }
+
     public function isAdmin(){
         return $this->roles()->where('name', 'admin')->exists();
     }
---
+++ database/migrations/2021_07_11_184442_group_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GroupUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('group_id');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_user');
+    }
+}
